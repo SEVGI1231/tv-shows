8f7e86f76c9326a58afd6c38a000426f2f81d7fd
Sevgi <sevgi2308@gmail.com>
2022-08-02 20:52:22 +0300

    data fetced episode selector created but not working

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,52 @@
 import { useState } from "react";
 import { EpisodeCard } from "./EpisodeCard";
-import episodes from "./episodes.json";
+//import episodes from "./episodes.json";
 import { Footer } from "./Footer";
 import "./App.css";
 import { searchBoxFunc } from "./utils/searchBoxFunc";
-import { EpisodeSelector } from "./EpisodeSelector";
+import { episodeCode } from "./utils/episodeCode";
+import Select from "react-select";
+import { useEffect } from "react";
+import { IEpisode } from "./types";
 
-console.log(`Imported ${episodes.length} episode(s)`);
-console.log(`First episode's name is ${episodes[0].name}`);
+//console.log(`Imported ${episodes.length} episode(s)`);
+//console.log(`First episode's name is ${episodes[0].name}`);
 function App(): JSX.Element {
+  const [episodes, setEpisodes] = useState<IEpisode[]>([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const response = await fetch("https://api.tvmaze.com/shows/82/episodes ");
+    const episodes: IEpisode[] = await response.json();
+    console.log(episodes);
+    setEpisodes(episodes);
+  };
+
   const [searchTerm, setSearchTerm] = useState<string>("");
   const filteredEpisodes = searchBoxFunc(searchTerm, episodes);
 
+  const option = episodes.map((episode) => ({
+    value: episode.id,
+    label: `S${episodeCode(episode.season)}E${episodeCode(episode.number)} - ${
+      episode.name
+    }`,
+    name: episode.name,
+    summary: episode.summary,
+    imageSrc: episode.image.medium,
+  }));
+  /*
+function handleOnChange()
+  
+  find selectedItem in episodes by usind id 
+  filteredEpisode= matched episode
+  return filteredEpisode
+  }
+*/
+
   return (
     <>
-      <EpisodeSelector />
+      <Select options={option} />
       <input
         type="text"
         value={searchTerm}
