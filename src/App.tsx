865f317e75990c6ad78718dded177b537a621a7f
Sevgi <sevgi2308@gmail.com>
2022-08-02 16:26:19 +0300

    working on episode selector

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import episodes from "./episodes.json";
 import { Footer } from "./Footer";
 import "./App.css";
 import { searchBoxFunc } from "./utils/searchBoxFunc";
+import {EpisodeSelector} from "./EpisodeSelector"
 
 console.log(`Imported ${episodes.length} episode(s)`);
 console.log(`First episode's name is ${episodes[0].name}`);
 function App(): JSX.Element {
   const [searchTerm, setSearchTerm] = useState<string>("");
   const filteredEpisodes = searchBoxFunc(searchTerm, episodes);
+
   return (
     <>
+      <EpisodeSelector/>
       <input
         type="text"
         value={searchTerm}
