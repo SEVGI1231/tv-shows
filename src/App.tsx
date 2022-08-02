29a2806b6879c0cd6e2022c027777765de7af22c
Sevgi <sevgi2308@gmail.com>
2022-08-02 10:03:54 +0300

    search box made tests needed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import { EpisodeCard } from "./EpisodeCard";
 import episodes from "./episodes.json";
 import { Footer } from "./Footer";
 import "./App.css";
+import {searchBoxFunc} from "./utils/searchBoxFunc"
 
 console.log(`Imported ${episodes.length} episode(s)`);
 console.log(`First episode's name is ${episodes[0].name}`);
 function App(): JSX.Element {
+  const [searchTerm, setSearchTerm]=useState<string>("")
+  const filteredEpisodes= searchBoxFunc(searchTerm, episodes)
   return (
     <>
+      <input type="text" value={searchTerm} 
+        onChange={(e)=>setSearchTerm(e.target.value)} 
+        placeholder="search here..."></input>
+      <span>Displaying {filteredEpisodes.length} out of {episodes.length}</span>
       <section className="episode-cards">
-        <div className="flex-box">{episodes.map(EpisodeCard)}</div>
+        <div className="flex-box">{filteredEpisodes.map(EpisodeCard)}</div>
       </section>
       <Footer />
     </>
---
+++ src/utils/searchBoxFunc.ts
@@ -0,0 +1,12 @@
+import { IEpisode } from "../types";
+
+export function searchBoxFunc(
+    searchTerm: string,
+    episodeData: IEpisode[]
+): IEpisode[]{
+    function callBackFunc(oneEpisodeData: IEpisode):boolean{
+        return (oneEpisodeData.name.toLowerCase().includes(searchTerm.toLowerCase())||
+            oneEpisodeData.summary.toLowerCase().includes(searchTerm.toLowerCase()))
+    }
+    return episodeData.filter(callBackFunc);
+}
