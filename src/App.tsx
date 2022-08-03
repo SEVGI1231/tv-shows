c5aec176fb381892542e818cdf36cc9aed5bd31a
Sevgi <sevgi2308@gmail.com>
2022-08-03 22:02:15 +0300

    level 350 completed with working DDmenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { useState } from "react";
 import { EpisodeCard } from "./EpisodeCard";
-//import episodes from "./episodes.json";
 import { Footer } from "./Footer";
 import "./App.css";
 import { searchBoxFunc } from "./utils/searchBoxFunc";
-import { episodeCode } from "./utils/episodeCode";
-import Select from "react-select";
+import { episodeCode2 } from "./utils/episodeCode";
 import { useEffect } from "react";
 import { IEpisode } from "./types";
 
-//console.log(`Imported ${episodes.length} episode(s)`);
-//console.log(`First episode's name is ${episodes[0].name}`);
 function App(): JSX.Element {
   const [episodes, setEpisodes] = useState<IEpisode[]>([]);
   useEffect(() => {
@@ -24,29 +20,51 @@ function App(): JSX.Element {
   };
 
   const [searchTerm, setSearchTerm] = useState<string>("");
-  const filteredEpisodes = searchBoxFunc(searchTerm, episodes);
+  const searchedEpisodes = searchBoxFunc(searchTerm, episodes);
 
-  const option = episodes.map((episode) => ({
-    value: episode.id,
-    label: `S${episodeCode(episode.season)}E${episodeCode(episode.number)} - ${
-      episode.name
-    }`,
-    name: episode.name,
-    summary: episode.summary,
-    imageSrc: episode.image.medium,
-  }));
-  /*
-function handleOnChange()
-  
-  find selectedItem in episodes by usind id 
-  filteredEpisode= matched episode
-  return filteredEpisode
+  const [episodeID, setEpisodeID] = useState<number | null>(null);
+  function handleOnChangeDD(e: React.ChangeEvent<HTMLSelectElement>) {
+    setEpisodeID(parseInt(e.target.value));
   }
-*/
-
+  function getSelectedEpisodeOrSearchedEpisodes(
+    episodeID: number | null,
+    filteredEpisodes: IEpisode[]
+  ): IEpisode[] {
+    if (episodeID === null) {
+      return filteredEpisodes;
+    } else {
+      const selectedEpisode: undefined | IEpisode = filteredEpisodes.find(
+        (episode) => episode.id === episodeID
+      );
+      const selectedEpisodeArr: IEpisode[] = [];
+      if (selectedEpisode !== undefined) {
+        selectedEpisodeArr.push(selectedEpisode);
+      }
+      return selectedEpisodeArr;
+    }
+  }
+  const displayedEpisodesData = getSelectedEpisodeOrSearchedEpisodes(
+    episodeID,
+    searchedEpisodes
+  );
+  function handleButtonClick() {
+    setEpisodeID(null);
+  }
   return (
     <>
-      <Select options={option} />
+      <label htmlFor="episode">
+        Select an episode
+        <select name="episode" id="episode" onChange={handleOnChangeDD}>
+          {episodes.map((episode: IEpisode, index) => {
+            return (
+              <option key={index} value={episode.id}>
+                {episodeCode2(episode)}-{episode.name}
+              </option>
+            );
+          })}
+        </select>
+      </label>
+      <button onClick={handleButtonClick}>Reset Selection</button>
       <input
         type="text"
         value={searchTerm}
@@ -54,10 +72,10 @@ function handleOnChange()
         placeholder="search here..."
       ></input>
       <span>
-        Displaying {filteredEpisodes.length} out of {episodes.length}
+        Displaying {searchedEpisodes.length} out of {episodes.length}
       </span>
       <section className="episode-cards">
-        <div className="flex-box">{filteredEpisodes.map(EpisodeCard)}</div>
+        <div className="flex-box">{displayedEpisodesData.map(EpisodeCard)}</div>
       </section>
       <Footer />
     </>
