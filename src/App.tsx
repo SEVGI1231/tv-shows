621457da1476cbf9c111c42cad60050cfe99986b
Sevgi <sevgi2308@gmail.com>
2022-08-05 13:03:43 +0300

    show card made working

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import "./App.css";
 //import { ListOfEpisodes } from "./components/ListOfEpisodes";
 import { ListOfShows } from "./components/ListOfShows";
+
 export default function App(): JSX.Element {
-  return <ListOfShows />;
+  return (
+    <>
+      <ListOfShows />;
+    </>
+  );
 }
--- src/components/ListOfShows.tsx
+++ src/components/ListOfShows.tsx
@@ -2,10 +2,6 @@ import shows from "../shows.json";
 import { ShowCard } from "./ShowCard";
 
 export function ListOfShows(): JSX.Element {
-  return (
-    <>
-      <p>Hello</p>
-      <div>{shows.map(ShowCard)}</div>
-    </>
-  );
+  console.log("rendering list of shows");
+  return <>{shows.map(ShowCard)}</>;
 }
